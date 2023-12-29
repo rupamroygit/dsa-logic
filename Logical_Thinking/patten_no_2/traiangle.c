d98f4ee7763fd898efebd86ad8453db35d4997a8
Rupam Roy <rupam.contact@gmail.com>
2023-12-29 20:13:04 +0530

    v-0.3
    new pattern add

---
+++ Logical_Thinking/patten_no_2/traiangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+    int num=5,i,j;
+
+    for(i=1;i<=num;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
